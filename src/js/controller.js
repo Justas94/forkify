479478e4f6894ffd5750c911bbb74de2120c8f63
Justas <46055975+Justas94@users.noreply.github.com>
2022-02-05 21:54:29 +0000

    Implemented search results & error messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,13 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
+if (module.hot) {
+  module.hot.accept();
+}
 const recipeContainer = document.querySelector(".recipe");
 
 // https://forkify-api.herokuapp.com/v2
@@ -30,8 +35,27 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    //render results
+    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
